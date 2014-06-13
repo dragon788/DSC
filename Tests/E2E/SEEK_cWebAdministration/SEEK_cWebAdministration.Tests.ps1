920abe32af8c9ccc48db03dfa2fa6bcb0f910849
Warner Godfrey <wgodfrey@seek.com.au>
2014-06-13 12:26:23 +1000

    E2E test coverage for all web administration resources

--- Tests/E2E/SEEK_cWebAdministration/SEEK_cWebAdministration.Tests.ps1
+++ Tests/E2E/SEEK_cWebAdministration/SEEK_cWebAdministration.Tests.ps1
@@ -62,15 +62,44 @@ Configuration TestConfiguration
             State = "Started"
             DependsOn = @("[cWebAppPool]TestAppPool")
         }
+
+        cWebApplication TestApplication
+        {
+            Website = "Test"
+            Name = "Test"
+            WebAppPool = "Test"
+            PhysicalPath = "C:\Temp\TestApplication"
+            Ensure = "Present"
+            AuthenticationInfo = SEEK_cWebAuthenticationInformation
+                                {
+                                    Anonymous = "true"
+                                    Basic = "false"
+                                    Digest = "false"
+                                    Windows = "false"
+                                }
+            DependsOn = @("[cWebsite]TestWebsite")
+        }
+
+        cWebVirtualDirectory TestVirtualDir
+        {
+            Name = "Virtual"
+            Website = "Test"
+            WebApplication = "Test"
+            PhysicalPath = "C:\Temp\TestVirtualDir"
+            DependsOn = @("[cWebApplication]TestApplication")
+        }
     }
 }
 
 Describe "WebSite DSC Resource" {
     Context "when web site is absent" {
 
-        Remove-WebSite "Test" -ErrorAction Ignore
         Remove-WebAppPool "Test" -ErrorAction Ignore
-        mkdir C:\inetpub\wwwroot\test | Out-Null
+        Remove-WebApplication -Site "Test" -Name "Test" -ErrorAction Ignore
+        Remove-WebSite "Test" -ErrorAction Ignore
+        mkdir C:\inetpub\wwwroot\test -ErrorAction Ignore | Out-Null
+        mkdir C:\Temp\TestVirtualDir -ErrorAction Ignore | Out-Null
+        mkdir C:\Temp\TestApplication -ErrorAction Ignore | Out-Null
         TestConfiguration -OutputPath .\tmp | Out-Null
         Start-DscConfiguration -Wait -Verbose -Path .\tmp
 
@@ -84,9 +113,20 @@ Describe "WebSite DSC Resource" {
             $WebsiteState.Value | Should Be "Started"
         }
 
+        It "creates a new web application" {
+            Get-WebApplication -Site "Test" -Name "Test" | Should Not Be $null
+        }
+
+        It "creates a new web application virtual directory " {
+            Get-WebVirtualDirectory -Site "Test" -Application "Test" -Name "Virtual" | Should Not Be $null
+        }
+
         Remove-Item -Recurse -Force .\tmp -ErrorAction Ignore | Out-Null
+        Remove-WebApplication -Site "Test" -Name "Test" -ErrorAction Ignore
         Remove-WebSite "Test" -ErrorAction Ignore
         Remove-WebAppPool "Test" -ErrorAction Ignore
         Remove-Item -Recurse -Force C:\inetpub\wwwroot\test
+        Remove-Item -Recurse -Force C:\Temp\TestVirtualDir
+        Remove-Item -Recurse -Force C:\Temp\TestApplication
     }
 }
