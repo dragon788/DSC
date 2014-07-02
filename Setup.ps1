b63ab1ec0f4bccbb3e049c71756d1331988b0e6f
Warner Godfrey <wgodfrey@seek.com.au>
2014-07-02 14:24:09 +1000

    unblock any powershell scripts in program files

--- Setup.ps1
+++ Setup.ps1
@@ -2,3 +2,5 @@ $DSCResourcesTarget = Join-Path $PSScriptRoot "Modules"
 $DSCResourcesRoot = Join-Path $env:ProgramFiles "WindowsPowerShell\Modules"
 
 Get-ChildItem $DSCResourcesTarget | foreach {cmd /c rmdir /S /Q "$DSCResourcesRoot/$_"; cmd /c mklink /j "$DSCResourcesRoot/$_" "$DSCResourcesTarget/$_"}
+
+Get-ChildItem -Path $DSCResourcesRoot -File -Recurse | Unblock-File
