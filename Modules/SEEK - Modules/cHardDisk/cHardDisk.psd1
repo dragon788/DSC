c55cfa5ff3df1041b3ec4a243d2d3f71e5ae84d5
Cameron Hine <chine@seek.com.au>
2014-08-25 15:42:04 +1000

    Add link resource for directory symlinks.

---
+++ Modules/SEEK - Modules/cHardDisk/cHardDisk.psd1
@@ -0,0 +1,31 @@
+@{
+# Version number of this module.
+ModuleVersion = '1.0.1'
+
+# ID used to uniquely identify this module
+GUID = 'f16bb534-c4e4-4887-b07d-4895a019c61d'
+
+# Author of this module
+Author = 'SEEK Ltd.'
+
+# Company or vendor of this module
+CompanyName = 'SEEK Ltd.'
+
+# Copyright statement for this module
+Copyright = '(c) 2014 SEEK Ltd. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'SEEK Module with DSC Resources for Web Administration'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '4.0'
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = '4.0'
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+}
---
+++ Tests/Unit/SEEK_cLink/SEEK_cLink.Tests.ps1
@@ -0,0 +1,70 @@
+$module = Join-Path $PSScriptRoot "..\..\..\Modules\Seek - Modules\cHardDisk\DSCResources\SEEK_cLink\SEEK_cLink.psm1"
+$code = Get-Content $module | Out-String
+Invoke-Expression $code
+
+Describe "Get-TargetResource" {
+    Context "when the link exists" {
+        Mock Test-Path { $true }
+
+        It "ensure is present" {
+            (Get-TargetResource -Link "existing-link").Ensure | Should Be "Present"
+        }
+    }
+
+    Context "when the link does not exsit" {
+        Mock Test-Path { $false }
+
+        It "ensure is absent" {
+            (Get-TargetResource -Link "not-a-link").Ensure | Should Be "Absent"
+        }
+    }
+}
+
+Describe "Test-TargetResource" {
+    Context "when the link exists" {
+        Mock Test-Path { $true }
+
+        It "is true, when ensure is present" {
+            (Test-TargetResource -Type "/D" -Link "existing-link" -Target "target") | Should Be $true
+        }
+
+        It "is false, when ensure is absent" {
+            (Test-TargetResource -Type "/D" -Link "existing-link" -Target "target" -Ensure "Absent") | Should Be $false
+        }
+    }
+
+    Context "when the link does not exist" {
+        Mock Test-Path { $false }
+
+        It "is false, when ensure is present" {
+            (Test-TargetResource -Type "/D" -Link "not-a-link" -Target "target") | Should Be $false
+        }
+
+        It "is true, when ensure is absent" {
+            (Test-TargetResource -Type "/D" -Link "not-a-link" -Target "target" -Ensure "Absent") | Should Be $true
+        }
+    }
+}
+
+Describe "Set-TargetResource" {
+    $testRoot = (Get-PSDrive TestDrive).Root
+    MkDir "${testRoot}\the-target" | Out-Null
+    Echo $null > "${testRoot}\the-target\a-file"
+    cmd /c mklink "/D" "C:\Temp\new-link" "${testRoot}\the-target" | Out-Null
+
+    Context "when desired state is absent" {
+        It "deletes the link" {
+            Set-TargetResource -Ensure "Absent" -Type "/D" -Link "C:\Temp\new-link" -Target "${testRoot}\the-target"
+            Test-Path "C:\Temp\new-link" | Should Be $false
+        }
+    }
+
+    Context "when desired state is present" {
+        It "makes the link" {
+            Set-TargetResource -Type "/D" -Link "C:\Temp\new-link" -Target "${testRoot}\the-target"
+            Test-Path "C:\Temp\new-link\a-file" | Should Be $true
+        }
+
+        cmd /c rmdir "C:\Temp\new-link"
+    }
+}
