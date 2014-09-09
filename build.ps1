a7c23ac7b50671d62ec36b03faead8a685a9ff34
Warner Godfrey <wgodfrey@seek.com.au>
2014-09-09 16:20:30 +1000

    build.ps1 returns a non-zero exit code if build fails

--- build.ps1
+++ build.ps1
@@ -11,3 +11,6 @@ $nuget = "${nugetHome}\NuGet.exe"
 & $nuget install .\.nuget\packages.config -OutputDirectory .\Packages -NonInteractive
 Import-Module (Join-Path $PSScriptRoot "Packages\psake.4.3.2\tools\psake.psm1")
 Invoke-Psake @args
+if (-not $psake.build_success) {
+  exit 1
+}
