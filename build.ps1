71b40d83685139245ec5b7ac7e257109e23c045e
Warner Godfrey <wgodfrey@seek.com.au>
2014-09-09 15:17:41 +1000

    changed package layout

--- build.ps1
+++ build.ps1
@@ -6,8 +6,8 @@ trap
   exit 1
 }
 
-$nugetHome = ".\Packages\NuGet.CommandLine.2.8.2\tools"
+$nugetHome = ".\.nuget\NuGet.CommandLine.2.8.2\tools"
 $nuget = "${nugetHome}\NuGet.exe"
-& $nuget install .\Packages\packages.config -OutputDirectory .\Packages -NonInteractive
+& $nuget install .\.nuget\packages.config -OutputDirectory .\Packages -NonInteractive
 Import-Module (Join-Path $PSScriptRoot "Packages\psake.4.3.2\tools\psake.psm1")
 Invoke-Psake @args
