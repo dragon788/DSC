b80bb4ffea70c77ec7437dd3ca4bd58bbc01da76
Warner Godfrey <wgodfrey@seek.com.au>
2014-11-06 16:39:34 +1100

    Added install step to clean-up manually installed resources

--- Tools/chocolateyInstall.ps1
+++ Tools/chocolateyInstall.ps1
@@ -2,6 +2,13 @@ try
 {
 	$DSCResourcesRoot = Join-Path $env:ProgramFiles "WindowsPowerShell\Modules"
 	$DSCResourceTarget = Join-Path $env:chocolateyPackageFolder "lib"
+
+	# remove DSC resources that may have been manually installed
+	if (Test-Path "$DSCResourcesRoot\SEEK - Modules") {
+		cmd /c rmdir "$DSCResourcesRoot\SEEK - Modules"
+	}
+
+	# remove previous installation of this package
 	Get-ChildItem $DSCResourceTarget | Foreach-Object {
 		if (Test-Path "$DSCResourcesRoot\$_") {
 			cmd /c rmdir "$DSCResourcesRoot\$_"
