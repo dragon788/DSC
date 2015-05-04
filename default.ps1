52eeee12b6da633f3147864de5be82367bab7392
Warner Godfrey <wgodfrey@seek.com.au>
2015-05-04 16:59:51 +1000

    choco pack works with 0.9.8 and 0.9.9

--- default.ps1
+++ default.ps1
@@ -16,10 +16,11 @@ task Package -depends Clean {
   if (-not (Test-Path $outputPackageDir)) {
     New-Item -ItemType directory -Path $outputPackageDir | Out-Null
   }
-  Get-ChildItem *.nuspec -Recurse | Foreach-Object {
+  Get-ChildItem "*.nuspec" -Recurse | Foreach-Object {
     Update-ModuleManifestVersion -Path $_.DirectoryName -Version $version -OutputDir $outputModuleManifestDir
-    # chocolatey pack expects a package name argument only, quotes are necessary to inject the additional OutputDir argument
-    exec { choco pack "$($_.FullName) -OutputDir $(Resolve-Path $outputPackageDir) -Version $version" }
+    Set-Location $outputPackageDir
+    exec { choco pack $($_.FullName) -Version $version }
+    Set-Location $PSScriptRoot
   }
 }
 
